a01f4f2cbd6eec8a55e7da9757a8bfd7372aca3c
Tushar12152 <tusharimran470@gmail.com>
2023-12-15 10:57:08 +0600

    contact section added

---
+++ src/Components/ContactUs.jsx
@@ -0,0 +1,77 @@
+import swal from "sweetalert";
+import Title from "../shared/Title";
+import Container from "../shared/Container";
+
+const Contact = () => {
+
+    const handleSend=e=>{
+         e.preventDefault()
+         swal("success","Your message is successFully send to Admin panel","success")
+    }
+
+
+    return (
+        <Container>
+   
+         <div className="my-10">
+         <Title heading={'Contact Us'}></Title>
+         </div>
+
+              <form onSubmit={handleSend}>
+          
+
+
+          <div className="flex w-full   gap-2">
+             
+        
+
+
+              <div className="form-control w-[50%]">
+                <label className="label">
+                  <span className="label-text">First Name</span>
+                </label>
+                <input  type="text" placeholder="First Name" className="input input-bordered border-pink-400 w-full" required />
+              </div>
+
+              <div className="form-control w-[50%]">
+                <label className="label">
+                  <span className="label-text">Last Name</span>
+                </label>
+                <input  type="text" placeholder="Last Name" className="input input-bordered border-pink-400 w-full" required />
+              </div>
+
+
+
+
+
+          </div>
+           
+
+          <div className="flex w-full   gap-2">
+             
+          <div className="form-control w-[50%]">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input name="description" type="text" placeholder="Email" className="input input-bordered border-pink-400 w-full " required />
+              </div>
+
+              <div className="form-control w-[50%]">
+                <label className="label">
+                  <span className="label-text">Phone</span>
+                </label>
+                <input name="description" type="text" placeholder="Phone" className="input input-bordered border-pink-400 w-full " required />
+              </div>
+
+          </div>
+
+         <textarea placeholder="Write Your Text.................." className="border-2 border-pink-400 w-full mt-6" name="" id="" cols="30" rows="10"></textarea>
+         
+
+             <input className="btn w-full mt-5 bg-pink-400 text-white" type="submit" value="Send" />
+        </form>
+        </Container>
+    );
+};
+
+export default Contact;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import AboutUs from "../Components/AboutUs";
 import Banner from "../Components/Banner";
+import Contact from "../Components/ContactUs";
 import OurCakes from "../Components/OurCakes";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
              <Banner></Banner>
              <OurCakes></OurCakes>
              <AboutUs/>
+             <Contact/>
         </div>
     );
 };
